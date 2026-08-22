fd2b2bff0b75c4e9c5d0ce2aa81411468a962bd4
Eric-weihaoyu <C250227@e.ntu.edu.sg>
2026-08-22 12:33:16 +0800

    chore: add production frontend origin to CORS

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://bookmark-api-omega.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
